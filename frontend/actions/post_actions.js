7b94f0f1c9aa0ddfe1cecf6cd83720d9fc639ccf
Eli <elicsiegel@gmail.com>
2017-08-19 12:43:31 -0400

    Set up basic post actions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,13 @@
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const RECEIVE_POST = 'RECEIVE_POST';
+
+export const receivePosts = posts => ({
+  type: RECEIVE_POSTS,
+  posts: posts 
+});
+
+export const receivePost = post => ({
+  type: RECEIVE_POST,
+  post: post 
+});
