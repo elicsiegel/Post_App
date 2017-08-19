f65f05c841cb11df23a5356fda961f27729ec665
Eli <elicsiegel@gmail.com>
2017-08-19 14:39:13 -0400

    Add root element

--- frontend/post_entry.jsx
+++ frontend/post_entry.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
 
 import { receivePost, receivePosts } from './actions/post_actions'; 
 
-// import Root from './components/root';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <HashRouter>
+      
+      <h1>Hello W</h1>
+    </HashRouter>
+  </Provider>
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store; 
   window.receivePost = receivePost;
   window.receivePosts = receivePosts; 
-  
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
